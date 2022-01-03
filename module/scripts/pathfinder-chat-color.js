a84c4672981d103c8813d15998635f520eb66409
Sasmira <sasmyra@gmail.com>
2022-01-03 01:28:52 +0100

    - fix errors with pathfinder-chat-color.js

--- module/scripts/pathfinder-chat-color.js
+++ module/scripts/pathfinder-chat-color.js
@@ -1,20 +1,4 @@
 Hooks.on('init', () => {
-	// Register module settings.
-	game.settings.register('dnd-ui', 'disableAllStyles', {
-		name: game.i18n.localize('RPGUI.SETTINGS.DISABLE_STYLES'),
-		hint: game.i18n.localize('RPGUI.SETTINGS.DISABLE_STYLES_HINT'),
-		scope: "client",
-		type: Boolean,
-		default: false,
-		config: true,
-		onChange: () => {
-			location.reload();
-		}
-	});
-
-	if (!game.settings.get('dnd-ui', 'disableAllStyles')) { rpgUIAddMainCss() }
-});
-Hooks.on('init', () => {
     // Register module settings.
 	game.settings.register('dnd-ui', 'icBgColor', {
 		name: game.i18n.localize('IC.BG.COLOR'),
@@ -91,7 +75,6 @@ Hooks.on('init', () => {
 });
 
 Hooks.on("renderChatLog", (log, html) => {
-	if (!game.settings.get('dnd-ui', 'disableChatColort'))	{
     // Prepend inline CSS to the chatlog to style the chat messages.
     icBgColor = game.settings.get('dnd-ui', 'icBgColor');
     icTextColor = game.settings.get('dnd-ui', 'icTextColor');
@@ -113,7 +96,6 @@ Hooks.on("renderChatLog", (log, html) => {
     "; color: " + otherTextColor + 
     " }\n #chat-log .message .message-header { color: " + otherTextColor + 
     "; } </style>").prependTo(html);
-	}
 });
 
 Hooks.on("chatMessage", (chatLog, message, chatData) => {
--- patchnotes.md
+++ patchnotes.md
@@ -2,6 +2,9 @@
 ## Compatibility
 - Ready for v9 version.
 
+## v9.238.4
+- fix errors with pathfinder-chat-color.js
+
 ## v9.238.3
 - Sidebar Macro Icon support added thank's you @Arcanist
 
